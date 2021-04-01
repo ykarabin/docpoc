1e51de7b0900e7fc86e48313c39ba36cebead516
ykarabin <ykarabin@gmail.com>
2021-04-01 10:47:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {DocViewer} from './DocViewer';
 
 function App() {
-    const [format, setFormat] = useState('docx');
+    const [format, setFormat] = useState('docxSample.docx');
 
     return (
         <div className="App ">
@@ -12,6 +12,12 @@ function App() {
                 <button onClick={()=>{setFormat('docxSample.xod')}}>docxSample.xod</button>
                 <button onClick={()=>{setFormat('pptxSample.pptx')}}>pptxSample.pptx</button>
                 <button onClick={()=>{setFormat('pptxSample.xod')}}>pptxSample.xod</button>
+                <button onClick={()=>{setFormat('pdfSample.pdf')}}>pdfSample.pdf</button>
+                <button onClick={()=>{setFormat('pdfSample.xod')}}>pdfSample.xod</button>
+                <button onClick={()=>{setFormat('xlsxSample.xlsx')}}>xlsxSample.xlsx</button>
+                <button onClick={()=>{setFormat('xlsxSample.xod')}}>xlsxSample.xod</button>
+                <button onClick={()=>{setFormat('xmlSample.xml')}}>xmlSample.xml</button>
+                <button onClick={()=>{setFormat('xmlSample.xod')}}>xmlSample.xod</button>
             </div>
             <div className="format">{format}</div>
             <DocViewer className="doc" url={`/docpoc/${format}`} />
